a57d8fef36aad7a31f85bf6d56835c16c5835baa
giuli-c <91190979+giuli-c@users.noreply.github.com>
2025-03-06 18:45:02 +0000

    modificati percorsi

--- Sentiment_Analysis/test/test_model.py
+++ Sentiment_Analysis/test/test_model.py
@@ -13,20 +13,29 @@ def test_model_training():
     Verifico che il file train.py venga eseguito correttamente e generi i file necessari per la predizione.
     """
     # Eseguo il training eseguendo train.py
-    result = subprocess.run(["python", "train.py"], capture_output=True, text=True)
+    # definisco la cartella in cui si trova il processo
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    train_script = os.path.join(root_dir, "train.py")
+
+    result = subprocess.run(["python", train_script], capture_output=True, text=True)
     assert result.returncode == 0, f"Train.py ha fallito la sua esecuzione"
 
     # Controllo che il file di training sia stato creato
-    assert os.path.exists("train_fasttext.txt"), "Errore: train_fasttext.txt non è stato creato."
+    train_file = os.path.join(root_dir, "train_fasttext.txt")
+    assert os.path.exists(train_file), "Errore: train_fasttext.txt non è stato creato."
 
     # Controllo che il modello sia stato salvato
-    assert os.path.exists("models/sentiment_model.ftz"), "Errore: Il modello non è stato salvato correttamente."
+    model_file = os.path.join(root_dir, "models/sentiment_model.ftz")
+    assert os.path.exists(model_file), "Errore: Il modello non è stato salvato correttamente."
 
     
 def test_model_prediction():
     """
     Verifico che il modello FastText riesca a fare predizioni.
     """
-    model = Model("models/sentiment_model.ftz")
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    model_path = os.path.join(root_dir, "models/sentiment_model.ftz")
+
+    model = Model(model_path)
     prediction = model.model.predict("Covid cases are increasing fast!")
     assert prediction is not None, "Errore: la predizione non è stata eseguita."
