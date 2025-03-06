4743ae05dd88c594ea4405c9ad1871626d3f426c
giuli-c <91190979+giuli-c@users.noreply.github.com>
2025-03-06 18:38:24 +0000

    Sistemato file di test

--- Sentiment_Analysis/test/test_model.py
+++ Sentiment_Analysis/test/test_model.py
@@ -1,23 +1,32 @@
 import sys 
 import os
+import pytest
+import subprocess
+
 # Aggiungo il percorso della cartella principale al PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import pytest
 from src.model import Model
 
 def test_model_training():
     """
-    Verifico che il modello FastText venga addestrato correttamente.
+    Verifico che il file train.py venga eseguito correttamente e generi i file necessari per la predizione.
     """
-    model = Model()
-    model.train("train_fasttext.txt")
-    assert model.model is not None, "Errore: il modello non è stato addestrato correttamente."
+    # Eseguo il training eseguendo train.py
+    result = subprocess.run(["python", "train.py"], capture_output=True, text=True)
+    assert result.returncode == 0, f"Train.py ha fallito la sua esecuzione"
 
+    # Controllo che il file di training sia stato creato
+    assert os.path.exists("train_fasttext.txt"), "Errore: train_fasttext.txt non è stato creato."
+
+    # Controllo che il modello sia stato salvato
+    assert os.path.exists("models/sentiment_model.ftz"), "Errore: Il modello non è stato salvato correttamente."
+
+    
 def test_model_prediction():
     """
     Verifico che il modello FastText riesca a fare predizioni.
     """
-    model = Model()
+    model = Model("models/sentiment_model.ftz")
     prediction = model.model.predict("Covid cases are increasing fast!")
     assert prediction is not None, "Errore: la predizione non è stata eseguita."
