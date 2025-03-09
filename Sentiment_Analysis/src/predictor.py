b444a8fec3a057b687683dd135252e6c794e1cf9
giuli-c <91190979+giuli-c@users.noreply.github.com>
2025-03-09 14:39:18 +0000

    Prova: rendere compatibile il testo per eseguire la predizione con fasttext.

--- Sentiment_Analysis/src/predictor.py
+++ Sentiment_Analysis/src/predictor.py
@@ -15,4 +15,12 @@ class SentimentPredictor:
         Esegue una predizione sul testo dato.
         """
         label, prob = self.model.predict(text)
+
+        # ERRORE: [*ValueError: Unable to avoid copy while creating an array as requested. If using `np.array(obj, copy=False)` replace it with `np.asarray(obj)`*] 
+        # Questo errore proviene da FastText, che sta tentando di usare np.array(obj, copy=False), ma NumPy 2.0 ha cambiato il comportamento di copy=False. 
+        # PRIMA: Creava un array senza copiare obj - Se obj non era già un array NumPy, veniva fatta una conversione automatica - Se obj non poteva essere usato direttamente, NumPy faceva una copia senza generare errori
+        # ORA: NumPy non copia più obj se non è già un array compatibile. Se è necessario copiare, invece di farlo automaticamente, genera un errore.
+        # >>>> Rimuozione di newline e spazi extra
+        text = text.strip().replace("\n", " ")
+
         return {"sentiment": label[0], "confidence": prob[0]}
