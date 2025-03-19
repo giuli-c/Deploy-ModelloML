06cb9ba6e0e6ad0383332321f3fd5a8176383c88
giuli-c <91190979+giuli-c@users.noreply.github.com>
2025-03-19 13:43:08 +0000

    Modifica del fiel predictor per consentire l'utilizzo della GPU.

--- Sentiment_Analysis/src/predictor.py
+++ Sentiment_Analysis/src/predictor.py
@@ -1,4 +1,4 @@
-from src.model import Model
+#from src.model import Model
 import torch
 
 class SentimentPredictor:
@@ -25,12 +25,22 @@ class SentimentPredictor:
     #     #text = text.strip().replace("\n", " ")
 
     #     return {"sentiment": label[0], "confidence": prob[0]}
-     def __init__(self, tokenizer, model):
+    def __init__(self, tokenizer, model):
         self.tokenizer = tokenizer
         self.model = model
+        # Controllo dove eseguire il modello
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
 
     def predict(self, text):
         inputs = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-        outputs = self.model(**inputs)
-        pred = outputs.logits.argmax(dim=1).item()
+        # Sposto gli input (tokenizzati) sullo stesso device del modello
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        # disattivo il calcolo del gradiente > per risparmiare memoria
+        with torch.no_grad():
+            # Passo gli input tokenizzati al modello e ottengo l’output
+            outputs = self.model(**inputs)
+            logits = outputs.logits
+            # Predizione della classe più alta
+            pred = torch.argmax(logits, dim=1).item()
         return pred
